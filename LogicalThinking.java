c5afabcfe54a939f1487c1424f25723e96a2b6d9
Sean <jesky.sean@outlook.com>
2016-12-04 09:29:37 -0700

    Update LogicalThinking.java

--- LogicalThinking.java
+++ LogicalThinking.java
@@ -6,28 +6,37 @@ package bools;
  */
 
 public class LogicalThinking {
-	public static void main(String args[]){		//we think, so speaking is irrelevant. disagreement causes conversations-
-		//question to the both people		//-agreement will silence the world. 
+	public static void main(String args[]){		
+		//we think, so speaking is irrelevant. disagreement causes conversations-
+		//-agreement will silence the world. 
+		
+		//question to the both people-
 		/*
 		 * what two numbers equal 2?**/
 		int person_IL = 2+1; //Illogical
 		int person_L = 1+1;  //Logical
 		
+		//asking if person_L shares the same answer as person_IL.
 		if (person_L == person_IL){
-			System.out.println("no problems here");		//asking if person_L shares the same answer as person_IL.
+			System.out.println("no problems here");		
 		}
+		 //if the IF( statement) returns false, print this.
 		else{
-			System.out.println("give up");		           //if the IF( statement) returns false, print this.
+			System.out.println("give up");		          
 			
 		}
+		 //if person_IL does not agree with person_L same result...
 		if(person_IL != person_L){
-			System.out.println(" still give up");		  //if person_IL does not agree with person_L same result...
+			System.out.println(" still give up");		 
 		}
+		///rare do we find no problems in the things we inquire
+		//about with others...
 		else{
-			System.out.println("no problems here");	         //rare do we find no problems in the things we inquire
-								         //about with others...
+			System.out.println("no problems here");	        
+								         
 		}
-		for(person_IL = person_IL; person_IL == 3; person_L++ ){     //if i decide to lie to myself, and stop the arguing. 
+		//if i decide to lie to myself, and stop the arguing. (to be dishonest with my hardwired belief.)
+		for(person_IL = person_IL; person_IL == 3; person_L++ ){     
 			System.out.println("congrats you lost your integerty,"
 					+ " by agreeing with somthing you dont agree with. ");
 		}
